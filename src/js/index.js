6d5a61c9abbf71cf970a71fd3c33ae8c0cd83252
Thales <furion2298@gmail.com>
2020-08-12 09:53:41 -0500

    commit desesperado

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,8 @@ import * as Detector from "../js/vendor/Detector";
 
 //Misti
 import  terrain from "../textures/mistiDEM.tif";
-import  mountainImage from "../textures/mistiMAPA.jpg";
+import  mountainImage from "../textures/mistiMAPA1.jpg";
+import  lavaImage from "../textures/lava.jpg";
 
 import * as GeoTIFF from "geotiff";
 
@@ -16,8 +17,15 @@ require("../sass/home.sass");
 
 class Application {
   constructor(opts = {}) {
+
+    this.count=1000;
+    this.distanciaA=9999;
+    this.maxg = new THREE.Vector3( 0, 0, -0.1 );
+    this.meta = new THREE.Vector3(-1000,82,-15);
+    
+
     this.width = window.innerWidth;
-    this.height = window.innerHeight
+    this.height = window.innerHeight;
 
     if (opts.container) {
       this.container = opts.container;
@@ -41,10 +49,12 @@ class Application {
   init() {
     this.scene = new THREE.Scene();
     this.raycaster = new THREE.Raycaster();
+    this.finterr=false;
     this.setupRenderer();
     this.setupCamera();
     this.setupControls();
     this.setupLight();
+    
     this.setupTerrainModel();
     this.setupEsfera();
     this.setupHelpers();
@@ -60,7 +70,78 @@ class Application {
 
   render() {
     this.controls.update();
-    //this.sphere.position.z += 0.01;
+    if(this.finterr)
+    {
+
+      //Algoritmo PSO
+
+      do
+      {
+        for (var i = 0 ; i < this.count ; i++) {
+          var esfera=this.balls[num];
+          // funcion objetivo
+          var objetivo=this.meta.distanceTo(esfera[0].position);
+          var 
+
+
+          //actualizar el maximo conocido
+
+
+          //actualizar el maximo global
+
+        }
+        //actualiza la tasa de inercia
+        for (var i = 0 ; i < this.count ; i++) {
+          //actualizar velocidad
+
+          //actualizar posicion
+
+        }
+      }
+      while(true);//condicion de satisfaccion
+
+
+      var num=Math.floor(Math.random()* (this.count - 0) + 0);
+      //for(var i = 0 ; i < this.count ; i++)
+      //{
+
+        var esfera=this.balls[num];
+        var opc=Math.floor(Math.random()* (4 - 0) + 0);
+        console.log("opc=",num);
+        var mov=Math.random()* (2 - 0.01) + 0.01;
+        
+
+        var vec=esfera[0].position.add(esfera[2].add(esfera[1].add(this.maxg)));
+        console.log("x=",vec.x);
+        console.log("z=",vec.z);
+        esfera[0].position.x=vec.x;
+        esfera[0].position.z=vec.z;
+
+        var raycaster = new THREE.Raycaster();
+        esfera[0].position.y=-600;
+        this.raycaster.set(esfera[0].position, new THREE.Vector3(0, 1, 0));
+
+        var intersects = this.raycaster.intersectObject(this.mountain);
+        esfera[0].position.y = intersects[0].point.y + 0.7;
+
+
+
+      //}
+
+
+    /*
+      this.sphere.position.z += 0.03;
+
+    var raycaster = new THREE.Raycaster();
+    this.raycaster.set(this.sphere.position, new THREE.Vector3(0, -1, 0));
+
+    var intersects = this.raycaster.intersectObject(this.mountain);
+    this.sphere.position.y = intersects[0].point.y + 0.7;
+
+    */
+    }
+    
+
     this.renderer.render(this.scene, this.camera);
     // when render is invoked via requestAnimationFrame(this.render) there is
     // no 'this', so either we bind it explicitly or use an es6 arrow function.
@@ -92,7 +173,7 @@ class Application {
     const near = 0.1;
     const far = 10000;
     this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this.camera.position.set(1000, 1000, 1000);
+    this.camera.position.set(500, 500, 500);
     this.camera.lookAt(this.scene.position);
   }
 
@@ -113,6 +194,7 @@ class Application {
 
   setupTerrainModel() {
     const readGeoTif = async () => {
+      console.log("Inicia terrain");
       const rawTiff = await GeoTIFF.fromUrl(terrain);
       const tifImage = await rawTiff.getImage();
       const image = {
@@ -136,7 +218,7 @@ class Application {
 
       console.time("parseGeom");
       geometry.vertices.forEach((geom, index) => {
-        geom.z = (data[index] / 20) * -1;
+        geom.z = (data[index] / 20) /* * 1 */;
       });
       console.timeEnd("parseGeom");
 
@@ -149,23 +231,39 @@ class Application {
 
       this.mountain = new THREE.Mesh(geometry, material);
       this.mountain.position.y = -100;
-      this.mountain.rotation.x = Math.PI / 2;
+      this.mountain.rotation.x = -Math.PI / 2;
 
       this.scene.add(this.mountain);
       this.mountain.updateMatrixWorld(true)
 
-      var sphere_geometry = new THREE.SphereGeometry(0.7, 32, 32);
-    var sphere_material = new THREE.MeshPhongMaterial({color: new THREE.Color(0.9, 0.55, 0.8)});
-    this.sphere = new THREE.Mesh(sphere_geometry, sphere_material);
-    this.sphere.position.y=10;
-    this.scene.add(this.sphere);
 
-    var raycaster = new THREE.Raycaster();
-    this.raycaster.set(this.sphere.position, new THREE.Vector3(0, 1, 0));
 
-    var intersects = this.raycaster.intersectObject(this.mountain);
-    this.sphere.position.y = intersects[0].point.y + 0.7;//radius of sphere
 
+      //esferas
+
+      
+
+      const pgeometry = new THREE.CircleGeometry( 11, 10);
+      const ptext= new THREE.TextureLoader().load(lavaImage);
+      const pmaterial = new THREE.MeshLambertMaterial( {
+          
+          wireframe: false,
+          side: THREE.DoubleSide,
+          map: ptext
+      });
+      
+      var plane = new THREE.Mesh( pgeometry, pmaterial );
+      //plane.position.y=182.5;
+
+      plane.position.y=400;
+      plane.position.x=82;
+      plane.position.z=-15;
+      plane.rotation.x=Math.PI/2;
+      this.scene.add( plane );
+
+
+      this.finterr= true;
+      console.log("fin terrain");
       const loader = document.getElementById("loader");
       loader.style.opacity = "-1";
 
@@ -183,6 +281,36 @@ class Application {
 
   setupEsfera()
   {
+    
+    this.balls = [];
+
+      for ( var i = 0 ; i < this.count ; i++ )
+      {
+        var interno=[];
+
+        //console.log("Inicia e");
+        var sphere_geometry = new THREE.BoxGeometry(0.7, 0.7, 0.7);
+        var sphere_material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        var sphere = new THREE.Mesh(sphere_geometry, sphere_material);
+        sphere.position.y=-600;
+
+        //plane.position.y=182.5;
+
+        //plane.position.y=400;
+        sphere.position.x=82;
+        sphere.position.z=-15;
+
+
+        interno.push(sphere);
+        interno.push(new THREE.Vector3(sphere.position.x, sphere.position.y, sphere.position.z));//maxlocal
+        interno.push(new THREE.Vector3(Math.random()* (1 - 0.1) + 0.1, 0, Math.random()* (1 - 0.1) + 0.1));//velocidad
+        this.balls.push(interno);
+
+
+        this.scene.add(sphere);
+        //console.log("fin e");
+        //radius of sphere    
+      }
     
 
   }
